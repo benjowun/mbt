c510aff554d67619d41a27cd695e801d8f339940
Benjamin Wunderling <benjamin.wunderling@student.tugraz.at>
2022-01-20 12:18:07 +0100

    Added PIN, design decisions explained in comments. Added messaging system for easier testing.

--- caralarm/CarAlarm.java
+++ caralarm/CarAlarm.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 
 
 
-// Current State: Requirement 6 + (7) + Logging, 
-// TODO: 7 could be done nicer
+// Current State: Requirement 1-3, 6+7, 4, TOPDO 5
 // TODO: add wait call if state not changed?
-// TODO: PIN code
+// TODO: messanging system --> show last message?
+// TODO: logged states are incorrect in other branches
+// TODO: Open should not open door if car is locked and alarm is activated?
 public class CarAlarm {
     private static boolean PRINT_DEBUG = true; // set to false to get rid of prints
+
+    public Message lastMessage = Message.None;
+
+    public enum Message {
+        None, InvalidPin, NewPinSet, InvalidDoor;
+    }
 
     class Door {
         public int number;
@@ -22,6 +29,59 @@ public class CarAlarm {
             this.isLuggage = isLuggage;
             this.isOpen = true; // NOTE: currently each door is open at start
             this.isLuggageLocked = false;
+        }
+    }
+
+    class PIN {
+        private int pincode;
+        public int errorCount;
+
+        private boolean isValidPIN(int pin) {
+            if (pin >= 10000 || pin < 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        public PIN(int pin) { // sets too default value if invalid PIN is entered
+            if (!isValidPIN(pin)) {
+                this.pincode = 1234;
+                lastMessage = Message.InvalidPin;
+            } else {
+                this.pincode = pin;
+            }
+            this.errorCount = 0;
+        }
+
+        public boolean matchesPIN(int pin) {
+            if (!isValidPIN(pin)) {
+                lastMessage = Message.InvalidPin;
+                this.errorCount++;
+                return false;
+            } else if (pin == this.pincode) {
+                this.errorCount = 0;
+                return true;
+            } else {
+                this.errorCount++;
+                return false;
+            }
+        }
+
+        // car must be unlocked
+        public boolean setPIN(int pin) {
+            if (!isValidPIN(pin)) {
+                lastMessage = Message.InvalidPin;
+                this.errorCount++;
+                return false;
+            } else if (matchesPIN(pin)) {
+                this.pincode = pin;
+                this.errorCount = 0;
+                lastMessage = Message.NewPinSet;
+                return true;
+            } else {
+                return false; // already incremented error in matchesPIN
+            }
         }
     }
 
@@ -136,7 +196,6 @@ public class CarAlarm {
             }
         };
 
-
         public State lock() { return this; }
         public State unlock() { return this; }
         public State close() { return this; }
@@ -149,6 +208,8 @@ public class CarAlarm {
     private ArrayList<Door> doors;
     private State currentState;
     private int waitCounter;
+    private PIN pincode;
+
 
     private boolean isNewState(State newState) {
         return newState != this.currentState;
@@ -160,7 +221,7 @@ public class CarAlarm {
                 return d;
         }
         
-        System.out.println("Error: invalid door."); //TODO: throw error
+        lastMessage = Message.InvalidDoor;
         return null;
     }
 
@@ -175,7 +236,7 @@ public class CarAlarm {
 
     private void log(String action, State nextState) {
         if (PRINT_DEBUG)
-            System.out.println("Moving from state: |" + this.currentState + "|  -->  " + action + "  |" + nextState + "|\n");
+            System.out.println("Moving from state: |" + this.currentState + "|  -->  " + action + "  -->  |" + nextState + "|\n");
     }
 
     private void debug() {
@@ -191,6 +252,7 @@ public class CarAlarm {
     public CarAlarm() {
         this.currentState = State.OpenAndUnlocked;
         this.doors = new ArrayList<>();
+        this.pincode = new PIN(1234); // set to default value, can be set
 
         Door door1 = new Door(1, false);
         Door door2 = new Door(2, false);
@@ -204,11 +266,6 @@ public class CarAlarm {
         doors.add(door4);
         doors.add(doorLuggage);
         //doors.add(door6);
-    }
-
-    // TODO: remove
-    public void test() {
-        System.out.println("Test");
     }
 
     public void Open(int door) {
@@ -229,7 +286,7 @@ public class CarAlarm {
 
     public void Close(int door) {
         Door carDoor = getDoor(door);
-        if (carDoor.isOpen == true) 
+        if (carDoor.isOpen == true && this.currentState != State.Alarm) // closing door that triggered alarm impossible
             carDoor.isOpen = false;
 
         debug();
@@ -247,7 +304,7 @@ public class CarAlarm {
         getDoor(5).isLuggageLocked = true;
 
         if (isNewState(this.currentState.lock())) {
-            log("CLOSE", this.currentState.lock());
+            log("LOCK", this.currentState.lock());
             this.currentState = this.currentState.lock();
             waitCounter = 0;
         }
@@ -262,9 +319,48 @@ public class CarAlarm {
             waitCounter = 0;
             return;
         } else if (isNewState(this.currentState.unlock())) {
-            log("CLOSE", this.currentState.unlock());
+            log("UNLOCK", this.currentState.unlock());
             this.currentState = this.currentState.unlock();
             waitCounter = 0;
+        }
+    }
+
+    // unlocks all doors
+    // @param: key - 4 digit pincode, TODO: throw error on invalid code
+    public void PinUnlock(int code) {
+        debug();
+        if (pincode.matchesPIN(code)) {
+            if (isNewState(this.currentState.unlock())) {
+                log("PINUNLOCK", this.currentState.unlock());
+                this.currentState = this.currentState.unlock();
+                waitCounter = 0;
+            }
+        } else if (pincode.errorCount >= 3) { // only reset errors on correct PIN!
+            if (currentState == State.Armed) {
+                // trigger alarm
+                log("PINUNLOCK", State.Alarm);
+                this.currentState = State.Alarm;
+                waitCounter = 0;
+            } // else do nothing
+        }
+    }
+
+    // sets the pincode to a new value
+    // requires being in unlocked state, on error, jump to alarm
+    // TODO: this introduces a whole bunch of weird new states we still have to think through
+    public void SetPinCode(int codeOld, int codeNew) {
+        debug();
+        if (this.currentState == State.OpenAndUnlocked) { // assumed Car must be open to set PIN as it is probably through internal display
+            pincode.setPIN(codeNew); 
+
+            if (pincode.errorCount >= 3) { // only reset errors on correct PIN!
+                // trigger alarm
+                log("SETPINCODE", State.Alarm);
+                this.currentState = State.Alarm;
+                waitCounter = 0;
+            }
+        } else {
+            // Don't throw error here, just doesnt do anything
         }
     }
 
@@ -273,7 +369,7 @@ public class CarAlarm {
         debug();
         waitCounter += seconds;
         if (this.currentState.getWaitTime() != 0 && waitCounter >= this.currentState.getWaitTime()) {
-            log("CLOSE", this.currentState.waitSecond());
+            log("WAIT", this.currentState.waitSecond());
             this.currentState = this.currentState.waitSecond();
             waitCounter = 0;
         }
@@ -285,5 +381,17 @@ public class CarAlarm {
 
     public int getWaitCounter() {
         return this.waitCounter;
+    }
+
+    public int getPinErrorCount() {
+        return this.pincode.errorCount;
+    }
+
+    public int getCurrentPin() {
+        return this.pincode.pincode;
+    }
+
+    public Message getLastMessage() {
+        return this.lastMessage;
     }
 }
